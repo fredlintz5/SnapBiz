7d83597fd54b69e8812da0e9f7defcb449ad539a
Fred Lintz <fredlintz@Freds-MacBook-Pro.local>
2017-11-04 20:13:56 -0600

    trying to deploy herkou again

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const bodyParser = require('body-parser');
 const express = require('express');
 const db = require("./models");
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 8080;
 
 
 app.use(fileUpload());
