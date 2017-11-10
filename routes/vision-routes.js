df7d3b506a9060aa4abbb01476d2e34c2e86c45d
Fred Lintz <fredlintz@Freds-MacBook-Pro.local>
2017-11-09 20:24:07 -0700

    credentials

--- routes/vision-routes.js
+++ routes/vision-routes.js
@@ -1,19 +1,25 @@
 const fs     = require('fs');
 const db     = require("../models");
+
+// fs.writeFile("/googleCreds.json", content, 'utf8', function (err) {
+//     if (err) {
+//         return console.log(err);
+//     }
+
+//     console.log("The file was saved!");
+// });
+
 const vision = require('@google-cloud/vision')({
   projectId: '3044e1b02e1804e67502cd943c6cf51f9f13d92b',
   // keyFilename: './googleServiceAccount.json'
-  keyFilename: JSON.parse(process.env.GOOGLE_CREDS)  
+  credentials: JSON.parse(process.env.GOOGLE_CREDS)  
 
 });
-
-console.log(JSON.parse(process.env.GOOGLE_CREDS));
 
 
 module.exports = (app) => {
 
   app.post('/upload', (req, res) => {
-      console.log(JSON.parse(process.env.GOOGLE_CREDS));
       if (!req.files) {
         return res.status(400).send('No files were uploaded.');
       } else {
